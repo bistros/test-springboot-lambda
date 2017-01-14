9bfaed3568ec3791a70d88ec0a4b2c4bb9c36b7f
tk <tk.kim@daumkakao.com>
2017-01-14 19:17:07 +0900

    improve

--- src/main/java/bistros/com/handler/BootHandler.java
+++ src/main/java/bistros/com/handler/BootHandler.java
@@ -4,18 +4,23 @@ import bistros.com.TestBootLambdaApplication;
 import bistros.com.component.LambdaBean;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.springframework.context.ApplicationContext;
 
 /**
  * Created by bistros on 2017. 1. 14..
  */
 public class BootHandler implements RequestHandler<String, String> {
+
+    ApplicationContext ac;
+
+    public BootHandler() {
+        ac = new TestBootLambdaApplication().getApplicationContext();
+    }
+
     @Override
     public String handleRequest(String input, Context context) {
-        TestBootLambdaApplication app = new TestBootLambdaApplication();
-
-        LambdaBean bean = app.getApplicationContext().getBean(LambdaBean.class);
+        LambdaBean bean = ac.getBean(LambdaBean.class);
         return bean.getVal();
-
     }
 }
 
